1ae4ae86ae0b61b9982e4e17a98008211978341c
@d.marakhovsky <mar.zzzx1@gmail.com>
2024-07-11 18:10:26 +0500

    Продолжение работы над листингом

--- Models/models.py
+++ Models/models.py
@@ -15,8 +15,8 @@ class Mom(models.Model):
     patronymic = models.CharField("Отчество", max_length=30)
     # Качества
     SEX = {
-        "M": "Male",
-        "F": "Female",
+        ("M", "Male"),
+        ("F", "Female"),
     }
 
     sex = models.CharField("Пол", choices=SEX)
@@ -41,6 +41,10 @@ class Dad(models.Model):
     surname = models.CharField("Фамилия", max_length=30)
     patronymic = models.CharField("Отчество", max_length=30)
     # Качества
+    SEX = {
+        ("M", "Male"),
+        ("F", "Female"),
+    }
     sex = models.CharField("Пол", choices=SEX)
     age = models.PositiveIntegerField("Возраст")
     eye_color = models.CharField("Цвет глаз", max_length=20)
@@ -59,23 +63,11 @@ class Dad(models.Model):
 
 
 class Child(Mom, Dad):
+    mom = models.OneToOneField(Mom, related_name="Мама", verbose_name="Ребенок")
+    dad = models.OneToOneField(Dad, related_name="Папа", verbose_name="Ребенок")
+
     class Meta:
         verbose_name = "Ребенок"
-
-
-class Wife(models.Model):
-    name = models.CharField("Имя", max_length=30)
-
-    class Meta:
-        verbose_name = "Жена"
-
-
-class Husband(models.Model):
-    name = models.CharField("Имя", max_length=30)
-    wife = models.OneToOneField(Wife, related_name="Жена", verbose_name="Mуж")
-
-    class Meta:
-        verbose_name = "Муж"
 
 
 class ClothingCompany(models.Model):
@@ -107,4 +99,80 @@ class TShirt(models.Model):
     class Meta:
         verbose_name = "Футболка"
         verbose_name_plural = "Футболки"
+
+
+class Park(models.Model):
+    name = models.CharField("Название", max_length=30)
+    address = models.CharField("Адрес", max_length=30)
+
+    class Meta:
+        verbose_name = "Парк"
+        verbose_name_plural = "Парки"
+
+
+class Breed(models.Model):
+    name = models.CharField("Название", max_length=30)
+
+    class Meta:
+        verbose_name = "Порода"
+        verbose_name_plural = "Породы"
+
+
+class Dog(models.Model):
+    name = models.CharField("Кличка", max_length=30)
+    breed = models.ForeignKey(Breed, related_name="Порода", verbose_name="Собака", on_delete=models.CASCADE)
+    park = models.ManyToManyField(Park, related_name="Парк", verbose_name="Собака")
+
+    class Meta:
+        verbose_name = "Собака"
+        verbose_name_plural = "Собаки"
+
+
+class Soul(models.Model):
+    HAIR_COLOR = {
+        ("Gi", "Рыжий"),
+        ("Bl", "Черный"),
+        ("Br", "Брюнет"),
+        ("Bol", "Лысый"),
+
+    }
+    hair_color = models.CharField("Цвет волос", choices=HAIR_COLOR)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if self.hair_color == "Gi":
+            return
+        else:
+            super().save(*args, **kwards)
+
+
+class PersonInfo(models.Model):
+    name = models.CharField("Имя", max_length=50)
+    age = models.PositiveIntegerField("Возраст")
+    SEX = {
+        ("M", "Male"),
+        ("F", "Female"),
+    }
+    sex = models.CharField("Пол", choices=SEX)
+
+    class Meta:
+        abstract = True
+
+
+class Student(PersonInfo):
+    group = models.CharField("Группа", max_length=10)
+
+    class Meta:
+        verbose_name = "Ученик"
+        verbose_name_plural = "Ученики"
+
+
+class Teacher(PersonInfo):
+    grade = models.CharField("Степень", max_length=15)
+
+    class Meta:
+        verbose_name = "Учитель"
+        verbose_name_plural = "Учителя"
+
 
